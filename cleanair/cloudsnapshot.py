7282260f2315b61f381a38ccbf23ae7276f0d084
Cher Simon <chersimon@Chers-MacBook-Pro.local>
2018-08-19 22:56:45 -0500

    List volumes and snapshots. Naive implementation of snapshot command

--- cleanair/cloudsnapshot.py
+++ cleanair/cloudsnapshot.py
@@ -14,8 +14,59 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Clean_Air manages snapshots"""
+
+@cli.group('volumes')
+def volumes():
+    """Command for volumes"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Command for snapshots"""
+
+@cli.group('instances')
 def instances():
     """Command for instances"""
+
+@volumes.command('list')
+@click.option('--project', default=None, help="Only volumes for project(tag Project: <name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(",".join((
+                v.id,
+                i.id,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted",
+            )))
+    return
+
+@snapshots.command('list')
+@click.option('--project', default=None, help="Only snapshots for project(tag Project: <name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    snappy = None
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(",".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    s.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))            
+    return
 
 @instances.command('list')
 @click.option('--project', default=None, help="Only instances for project(tag Project: <name>)")
@@ -36,6 +87,19 @@ def list_instances(project):
             tags.get('Project','<no project>')
         )))
 
+@instances.command('snapshot', help='Create snapshots of all volumes')
+@click.option('--project', default=None, help='Only instances for the project')
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshots of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Clean Air")
+    return
 
 @instances.command('start')
 @click.option('--project', default=None, help='Only instances for the project')
@@ -64,4 +128,4 @@ def stop_instances(project):
 
 
 if __name__ == '__main__':
-    instances()
+    cli()
