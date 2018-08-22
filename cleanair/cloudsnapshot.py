91b25aee80f90c4d3597ba015baedf9e1a25c7cc
Cher Simon <chersimon@Chers-MacBook-Pro.local>
2018-08-22 04:42:41 -0500

    Don't start another snapshot when there is already one in progress

--- cleanair/cloudsnapshot.py
+++ cleanair/cloudsnapshot.py
@@ -14,6 +14,10 @@ def filter_instances(project):
         instances = ec2.instances.all()
     return instances
 
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Clean_Air manages snapshots"""
@@ -105,6 +109,9 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshots(v):
+                print("Skipping {0}, snapshots already in progress".format(v.id))
+                continue
             print("Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description="Created by Clean Air")
         i.start()
